81ab2eb4f2d61560987928b14570e1b2f6bc85ef
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-03-29 11:03:07 -0300

    segundo commit aula3 timestamp 56mim

--- src/App.java
+++ src/App.java
@@ -1,11 +1,6 @@
 import java.io.File;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -14,25 +9,21 @@ public class App {
         // Fazer uma conexão HTTP e armazenar o top 250 filmes numa String
         // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
 
-        // Extrair só os dados que interessam (titulo, poster, nota)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         // Exibir e manipular os dados
         var geradora = new GeradoraDeFigurinhas();
         var diretorio = new File("saida/");
         diretorio.mkdir();
-        for (int i = 0; i < 3; i++) {
-            Map<String,String> filme = listaDeFilmes.get(i);
 
-            String urlImagem = filme.get("url").replaceAll("(@+)(.*).jpg$", "$1.jpg");
-            String titulo = filme.get("title");
+        for (int i = 0; i < 3; i++) {
+            Map<String,String> conteudo = listaDeConteudos.get(i);
+
+            // conteudo.get("image")
+            String urlImagem = conteudo.get("url").replaceAll("(@+)(.*).jpg$", "$1.jpg");
+            String titulo = conteudo.get("title");
 
             InputStream inputStream = new URL(urlImagem).openStream();
             String nomeArquivo = "saida/" + titulo.replace(":","") + ".png";
---
+++ src/ExtratorConteudoNasa.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorConteudoNasa {
+    public List<Conteudo> extraionteudos (String json) {
+        // Extrair só os dados que interessam (titulo, poster, nota)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeAtributos = parser.parse(json);
+
+        List<Conteudo> conteudos = new ArrayList<>();
+
+        // Popular a lista de conteudos
+        for (Map<String, String> atributos : listaDeAtributos) {
+            String titulo = atributos.get("title");
+            String urlImagem = atributos.get("url");
+            var conteudo = new Conteudo(titulo, urlImagem);
+
+            conteudos.add(conteudo);
+        }
+
+        return conteudos;
+    }
+}
