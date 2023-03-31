2aecc005d3c647fa1338208512b8ae7dff86282b
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-03-31 10:53:49 -0300

    commit CRUD completo

--- linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,5 +42,9 @@ public class LinguagemController {
         linguagem.setId(id);
         Linguagem linguagemSalva = repositorio.save(linguagem);
         return linguagemSalva;
+    }
+
+    @DeleteMapping("/linguagens/{id}") public void excluirLinguagem(@PathVariable String id) {
+        repositorio.deleteById(id);
     }
 }
